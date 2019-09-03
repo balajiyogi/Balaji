9a57104104e8e08d922115182037355dd0ef7886
balajiyogi <54831242+balajiyogi@users.noreply.github.com>
2019-09-03 11:55:37 +0530

    Update triangle1.py

--- triangle1.py
+++ triangle1.py
@@ -1,7 +1,8 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if(a+b <= c or a+c <= b or b+c <= a):
+a,b,c = input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+if(a**2 + b**2 == c**2):
   print("yes")
 else:
   print("no")
